975df313582dfafc6dd8717cefadeb706ea620c3
Yana Kutsko <yk03012014@gmail.com>
2019-09-28 00:52:20 +0300

    added user service

---
+++ BikeRide/Service/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeRide.Service
+{
+    public interface IUserService
+    {
+        Task<bool> ValidateCredentials(string username, string password, out User user);
+    }
+
+    public class User
+    {
+        public User(string username)
+        {
+            Username = username;
+        }
+
+        public string Username {get;}
+    }
+}
--- BikeRide/Startup.cs
+++ BikeRide/Startup.cs
@@ -1,3 +1,4 @@
+using BikeRide.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 
 namespace BikeRide
 {
@@ -17,18 +19,18 @@ namespace BikeRide
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            var users = new Dictionary<string, string> { { "YanaKutsko", "1092002" } };
+            services.AddSingleton<IUserService>(new UserService(users));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,7 +40,6 @@ namespace BikeRide
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -60,9 +61,6 @@ namespace BikeRide
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
