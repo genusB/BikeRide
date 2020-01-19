07832e3986257643dff23cb765d4ae42368a3424
genusB <52314405+genusB@users.noreply.github.com>
2020-01-19 15:40:20 +0300

    main part updated

--- src/Spa/oauth-client/src/app/app-routing.module.ts
+++ src/Spa/oauth-client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
+ 
 
 const routes: Routes = [
   { path: 'auth-callback', component: AuthCallbackComponent  },
--- src/Spa/oauth-client/src/app/app.module.ts
+++ src/Spa/oauth-client/src/app/app.module.ts
@@ -4,21 +4,31 @@ import { HttpClientModule } from '@angular/common/http';
 
 // used to create fake backend
 import { FakeBackendProvider } from './shared/mocks/fake-backend-interceptor';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ConfigService } from './shared/config.service';
+
 import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
 
 
 /* Module Imports */
 import { CoreModule } from './core/core.module';
-import { HomeModule }  from './home/home.module';
-import { AccountModule }  from './account/account.module';
+import { HomeModule } from './home/home.module';
+import { AccountModule } from './account/account.module';
 import { ShellModule } from './shell/shell.module';
 import { TopSecretModule } from './top-secret/top-secret.module';
+import { SharedModule } from './shared/shared.module';
 import { MapModule} from './map/map.module';
-import { SharedModule }   from './shared/shared.module';
 import { MeetingModule } from './meeting/meeting.module';
+import { BadmakerModule } from './badmaker/badmaker.module';
+import { MathModule } from './mathGame/math.module';
+import { TttModule } from './tic-tac-toe/ttt.module';
+import { GalleryModule } from './gallery/gallery.module';
+import { SnakeModule } from './snake/snake.module';
+import { BrickModule } from './brick-breaker/brick.module';
+
 
 @NgModule({
   declarations: [
@@ -32,11 +42,17 @@ import { MeetingModule } from './meeting/meeting.module';
     HomeModule,
     MapModule,
     MeetingModule,
+    BadmakerModule,
+    MathModule,
+    TttModule,
+    GalleryModule,
+    SnakeModule,
+    BrickModule,
     AccountModule,
     TopSecretModule,
     AppRoutingModule,
     ShellModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [
     ConfigService,
